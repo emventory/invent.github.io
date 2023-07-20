fe133e7a4893ff72275c3743c3857f64e7b0225f
emventory <139507983+emventory@users.noreply.github.com>
2023-07-20 12:48:52 +0100

    Add files via upload

--- js/salestransdetails.js
+++ js/salestransdetails.js
@@ -25,17 +25,32 @@
   // Get a reference to the database service
   const db = getDatabase();
   
-  function getAllTransactions(){
+  function getTransactionsSalesDetails(){
 
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
 
     const rep_id = urlParams.get('rep_id');
     const rep_name = urlParams.get('rep_name');
+    const t_Id = urlParams.get('t_Id');
+    const discount = urlParams.get('discount');
+    const mode_of_pay = urlParams.get('mode_of_pay');
+    const userid = urlParams.get('userid');
+
+    const amount_tendered = urlParams.get('amttendered');
+    const remaining_balance = urlParams.get('remainbalance');
 
     document.getElementById("repNameH6").innerText = rep_name;
     document.getElementById("repId").value = rep_id;
     document.getElementById("repName").value = rep_name;
+    document.getElementById("tidHidden").value = t_Id;
+
+    document.getElementById("discountHidden").value = discount;
+    document.getElementById("paymentmodeHidden").value = mode_of_pay;
+    document.getElementById("useridHidden").value = userid;
+
+    document.getElementById("amttenderedHidden").value = amount_tendered;
+    document.getElementById("remainbalanceHidden").value = remaining_balance;
   
   
   //document.getElementById("repIdHidden").value = rep_id;
@@ -43,20 +58,20 @@
       var status = 1;
       console.log(rep_id);
       
-      const que = query(ref(db,'transactions'),orderByChild('sales_rep_id'),equalTo(rep_id));
+      const que = query(ref(db,'sales'),orderByChild('t_id'),equalTo(t_Id));
 
       get(que).then((snapshot) => {
         if (snapshot.exists()) {
 
-          var transactions = [];
+          var transactionsSales = [];
 
           snapshot.forEach(function (childSnapshot) {
 
-            transactions.push(childSnapshot.val());
+            transactionsSales.push(childSnapshot.val());
 
         });
 
-        addTransactionsForDisplay(transactions);
+        addTransactionsSalesForDisplay(transactionsSales);
 
         //console.log(snapshot.val().name);
         } else {
@@ -69,48 +84,53 @@
 							
 }
 
-window.onload = getAllTransactions();
+window.onload = getTransactionsSalesDetails();
 
 var tAmount = 0;
-var tRemAmount = 0;
-var tAmountTendered = 0;
-var tDiscount = 0;
+var tRemAmount = document.getElementById("remainbalanceHidden").value;
+var tAmountTendered = document.getElementById("amttenderedHidden").value;
+var tDiscount = document.getElementById("discountHidden").value;
 //var tAfterDiscount = 0;
 
-function  addTransactionsForDisplay(transactions){
-  transactions.forEach(element => {
+function  addTransactionsSalesForDisplay(transactionsSales){
+  transactionsSales.forEach(element => {
 
-    sendToDisplay(element.t_id, element.user_id, element.sales_rep_id, 
-      element.total_amount, element.total_items, element.amount_tendered, 
-      element.remaining_balance, element.discount, element.mode_of_pay, 
-      element.payment_status, element.t_date, element.status);
+    sendToDisplay(element.sales_id, element.t_id, element.sales_rep_id, 
+      element.product_id, element.product_qty, element.total_amount, 
+      element.unit_price, element.user_id, element.date,element.status);
+
+      //tAmount = Number(element.total_amount);
 
       tAmount = Number(element.total_amount) + tAmount;
-      tRemAmount = Number(element.remaining_balance) + tRemAmount;
-      tAmountTendered = Number(element.amount_tendered) + tAmountTendered;
-      tDiscount = Number(element.discount) + tDiscount;
+      // tRemAmount = Number(element.remaining_balance) + tRemAmount;
+      // tAmountTendered = Number(element.amount_tendered) + tAmountTendered;
+      //tDiscount = Number(element.discount) + tDiscount;
       //tAfterDiscount = Number((tAmount - tDiscount)) + tAfterDiscount;
       //console.log(tAmount);
 
   });
   console.log(tAmount);
 
+  //document.getElementById("discountHidden").value
+
   if(tRemAmount > 0){   
+
+
 
     document.getElementById('totalDisplay').classList.add('btn','btn-danger','btn-sm','w-100','shadow')
     document.getElementById('totalDisplay').innerText = "Accum. Total: \u20A6"+tAmount+
-    "\n Total Discount: \u20A6"+ tDiscount+
+    "\n Discount: \u20A6"+ tDiscount+
     "\n Total After Discount: \u20A6"+ (tAmount - tDiscount)+
-    "\n Total Amount Tendered: \u20A6"+ tAmountTendered+"\n Total Remaining Bal: \u20A6"+ tRemAmount;
+    "\n Amount Tendered: \u20A6"+ tAmountTendered+"\n Remaining Bal: \u20A6"+ tRemAmount;
 
   }
   else if(tRemAmount <= 0){
 
     document.getElementById('totalDisplay').classList.add('btn','btn-success','btn-sm','w-100','shadow')
     document.getElementById('totalDisplay').innerText = "Accum. Total: \u20A6"+tAmount+
-    "\n Total Discount: \u20A6"+ tDiscount+
+    "\n Discount: \u20A6"+ tDiscount+
     "\n Total After Discount: \u20A6"+ (tAmount - tDiscount)+
-    "\n Total Amount Tendered: \u20A6"+ tAmountTendered+"\n Total Remaining Bal: \u20A6"+ tRemAmount;
+    "\n Amount Tendered: \u20A6"+ tAmountTendered+"\n Remaining Bal: \u20A6"+ tRemAmount;
     
   }
  
@@ -122,9 +142,8 @@ var bg_white = document.getElementById('bg_white');
 
 var radioId = 1;
 
-function sendToDisplay(t_id, user_id, sales_rep_id, total_amount, total_items, 
-  amount_tendered, remaining_balance, discount, mode_of_pay, payment_status, 
-  t_date, status){
+function sendToDisplay(sales_id, t_id, sales_rep_id, product_id, product_qty, total_amount, 
+  unit_price, user_id, date, status){
 
   let btn_check = document.createElement('input');
   btn_check.setAttribute('type', 'radio');
@@ -145,7 +164,7 @@ function sendToDisplay(t_id, user_id, sales_rep_id, total_amount, total_items,
 
   btn_outline_lightLabel.setAttribute('data-bs-toggle', 'tooltip');
   btn_outline_lightLabel.setAttribute('data-bs-placement', 'bottom');
-  btn_outline_lightLabel.setAttribute('title', 'Remaining Balance : '+remaining_balance);
+  
 
 	btn_outline_lightLabel.classList.add ('btn','btn-outline','d-flex','align-items-center','gap-3','rounded','px-3','py-2','mt-2');
 
@@ -153,12 +172,12 @@ function sendToDisplay(t_id, user_id, sales_rep_id, total_amount, total_items,
   let bi_houseItalics = document.createElement('i');
 	//bi_houseItalics.classList.add ('bi','bi-ui-checks-grid','text-success');
 
-  if(remaining_balance == 0){
+  //if(remaining_balance == 0){
     bi_houseItalics.classList.add ('bi','bi-ui-checks-grid','text-success');
-  }
-  else if(remaining_balance > 0){
-    bi_houseItalics.classList.add ('bi','bi-ui-checks-grid','text-danger');
-  }
+  //}
+  //else if(remaining_balance > 0){
+    //bi_houseItalics.classList.add ('bi','bi-ui-checks-grid','text-danger');
+  //}
   //
   btn_outline_lightLabel.appendChild(bi_houseItalics);
 
@@ -168,40 +187,75 @@ function sendToDisplay(t_id, user_id, sales_rep_id, total_amount, total_items,
   let fw_boldH6 = document.createElement('h6');
 	fw_boldH6.classList.add ('mb-0','fw-bold');
 
-  let h6Text = document.createTextNode("Transac. Amount: \u20A6"+ Intl.NumberFormat('en-US').format(total_amount));
-  fw_boldH6.appendChild(h6Text);
+
+
+
+
+ 
+  let p_Name;
+  const getProductNameQue = query(ref(db,'stock'),orderByChild('product_id'),equalTo(product_id));
+
+  get(getProductNameQue).then((snapshot) => {
+    if (snapshot.exists()) {
+
+      var productNames = [];
+
+      snapshot.forEach(function (childSnapshot) {
+
+        p_Name = childSnapshot.val().product_name;
+        let h6Text = document.createTextNode("Product: "+p_Name);
+        fw_boldH6.appendChild(h6Text);
+
+        btn_outline_lightLabel.setAttribute('title', p_Name+ '\n Total: '+total_amount);
+
+    });
+
+    //addTransactionsSalesForDisplay(productNames);
+
+    //console.log(snapshot.val().name);
+    } else {
+      alert("Sorry! No Transaction(s) Yet For This Sales Rep");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+
+  
+
+
+
 
   text_startSpan.appendChild(fw_boldH6);
 
   let text_mutedDiv = document.createElement('div');
 	text_mutedDiv.classList.add ('small', 'text-dark');
 
-  let text_mutedDivNoItemsText = document.createTextNode('No. Of Items: '+total_items);
+  let text_mutedDivNoItemsText = document.createTextNode('Quantity: '+product_qty);
   text_mutedDiv.appendChild(text_mutedDivNoItemsText);
 
   let br = document.createElement('br');
   text_mutedDiv.appendChild(br);
 
 
-  let text_mutedDivAmtTendered = document.createTextNode('Amount Tendered: \u20A6'+amount_tendered);
+  let text_mutedDivAmtTendered = document.createTextNode('Unit Price: \u20A6'+unit_price);
   text_mutedDiv.appendChild(text_mutedDivAmtTendered);
 
   let br2 = document.createElement('br');
   text_mutedDiv.appendChild(br2);
 
-  let text_mutedDivDiscount = document.createTextNode('Discount: \u20A6'+discount);
+  let text_mutedDivDiscount = document.createTextNode('Total: \u20A6'+total_amount);
   text_mutedDiv.appendChild(text_mutedDivDiscount);
 
   let br3 = document.createElement('br');
   text_mutedDiv.appendChild(br3);
 
-  let text_mutedRemainingBal = document.createTextNode('Remaining Bal.: \u20A6'+remaining_balance);
-  text_mutedDiv.appendChild(text_mutedRemainingBal);
+  // let text_mutedRemainingBal = document.createTextNode('Remaining Bal.: \u20A6'+remaining_balance);
+  // text_mutedDiv.appendChild(text_mutedRemainingBal);
 
-  let br4 = document.createElement('br');
-  text_mutedDiv.appendChild(br4);
+  // let br4 = document.createElement('br');
+  // text_mutedDiv.appendChild(br4);
 
-  let text_mutedDivTransDate = document.createTextNode(' Transac. Date: '+t_date.slice(0, -36));
+  let text_mutedDivTransDate = document.createTextNode(' Transac. Date: '+date.slice(0, -36));
   text_mutedDiv.appendChild(text_mutedDivTransDate);
 
   text_startSpan.appendChild(text_mutedDiv);
@@ -250,12 +304,7 @@ function sendToDisplay(t_id, user_id, sales_rep_id, total_amount, total_items,
   el2.addEventListener("click", function(){
     //rep_id, rep_name, rep_phone, rep_email, rep_address, date_registered
       //console.log(el.value);
-      window.open('salestransactiondetails.html?rep_id='+document.getElementById("repId").value+
-      '&rep_name='+document.getElementById("repName").value+'&t_Id='+t_id+'&discount='+discount+
-      '&paymentmode='+mode_of_pay+
-      '&userid='+user_id+
-      '&amttendered='+amount_tendered+
-      '&remainbalance='+remaining_balance, "_self");
+      window.open('salestransactiondetails.html?rep_id='+document.getElementById("repId").value+'&rep_name='+document.getElementById("repName").value, "_self");
     }
   )
   radioId = radioId+1;
